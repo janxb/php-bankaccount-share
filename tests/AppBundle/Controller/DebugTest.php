a882ee5050d16c55a4a7cc5146d86cd82a22da04
janxb <github@janbrodda.de>
2017-06-06 17:13:10 +0200

    moved default symfony test

--- tests/AppBundle/Controller/DebugTest.php
+++ tests/AppBundle/Controller/DebugTest.php
@@ -4,15 +4,15 @@ namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class DebugTest extends WebTestCase
 {
     public function testIndex()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/debug');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertContains('foo', $crawler->filter('#foo')->text());
     }
 }
